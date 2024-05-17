e4f9487611502872de3c02c931f08bf054735a6e
Ruslan Bikineev <biken_r@mail.ru>
2024-05-17 15:52:12 +0300

    Ruslan Bikineev: change allure annotation

--- UI-autotests/src/test/java/tests/sql/exercises/SqlExercisesPageTests.java
+++ UI-autotests/src/test/java/tests/sql/exercises/SqlExercisesPageTests.java
@@ -19,11 +19,11 @@ import static data.TestsData.PATH_TO_COOKIES_FILE;
 import static io.qameta.allure.SeverityLevel.NORMAL;
 
 @Epic(value = "Главная страница")
-@Feature(value = "Авторизация")
 public class SqlExercisesPageTests extends BaseTest {
     private Json json = new Json();
 
     @Test
+    @Feature(value = "Авторизация пользователем гость")
     @Story(value = "Авторизация пользователем гость и сохранение Cookie в файл")
     @Owner(value = "Ruslan Bikineev")
     @Severity(NORMAL)
@@ -36,6 +36,7 @@ public class SqlExercisesPageTests extends BaseTest {
     }
 
     @Test(dependsOnMethods = "testAuthorizationWithoutLoginAndSaveCookiesToFile")
+    @Feature(value = "Авторизация пользователем гость")
     @Story(value = "Авторизация пользователем гость c использованием Cookie")
     @Owner(value = "Ruslan Bikineev")
     @Severity(NORMAL)
