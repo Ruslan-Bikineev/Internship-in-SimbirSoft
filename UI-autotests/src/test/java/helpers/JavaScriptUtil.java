9d6ad6349a85a72372d613fc78ebff980861a5eb
Ruslan Bikineev <biken_r@mail.ru>
2024-05-20 23:27:59 +0300

    Ruslan Bikineev: added to check HeightScroll in page

--- UI-autotests/src/test/java/helpers/JavaScriptUtil.java
+++ UI-autotests/src/test/java/helpers/JavaScriptUtil.java
@@ -12,12 +12,16 @@ public class JavaScriptUtil {
         js.executeScript("arguments[0].blur();", element);
     }
 
-    @Step("Наличие скролла на странице")
-    public static boolean hasScroll(WebDriver driver) {
-        boolean result = false;
+    @Step("Наличие скролла по высоте страницы")
+    public static boolean hasHeightScroll(WebDriver driver) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        int resultHeight = (int) js.executeScript("return document.body.scrollHeight;");
-        System.out.println(resultHeight);
-        return result;
+        // Высота видимой части документа
+        long visibleHeight = (long) (js.executeScript("return document.documentElement.clientHeight"));
+        // Высота всего документа со всей прокручиваемой областью страницы
+        long fullHeight = (long) js.executeScript("return Math.max(document.body.scrollHeight, " +
+                "document.documentElement.scrollHeight, document.body.offsetHeight, " +
+                "document.documentElement.offsetHeight, document.body.clientHeight, " +
+                "document.documentElement.clientHeight)");
+        return fullHeight > visibleHeight;
     }
 }
