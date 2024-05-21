bd26472a4df19e9ff570202511ac55f165f90d2a
Ruslan Bikineev <biken_r@mail.ru>
2024-05-21 12:50:45 +0300

    Ruslan Bikineev: added remote driver and new profile for run parallel tests, TODO: bat file for run grid

--- UI-autotests/src/test/java/tests/BaseTest.java
+++ UI-autotests/src/test/java/tests/BaseTest.java
@@ -2,10 +2,13 @@ package tests;
 
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.List;
 
@@ -13,8 +16,9 @@ public class BaseTest {
     private WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
-        this.driver = new ChromeDriver();
+    public void setUp() throws MalformedURLException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        this.driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeOptions);
         this.driver.manage().window().maximize();
         this.driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
         this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
