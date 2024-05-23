ab488ec54942b2accb269bbcc20dec7d8d3a3927
Ruslan Bikineev <biken_r@mail.ru>
2024-05-23 10:46:33 +0300

    Ruslan Bikineev: move selenium-server to project file and change scripts

--- UI-autotests/src/test/java/helpers/scripts/start-grid.bat
+++ UI-autotests/src/test/java/helpers/scripts/start-grid.bat
@@ -1,9 +1,11 @@
 @echo off
-SET SELENIUM_DIR=C:\Program Files\Selenium-server-4.21.0
+SET SELENIUM_DIR=selenium-server
 SET SELENIUM_JAR=selenium-server-4.21.0.jar
-cd /
+
 cd %SELENIUM_DIR%
+
 start java -jar %SELENIUM_JAR% hub
 TIMEOUT /t 5 /nobreak
+
 start java -jar %SELENIUM_JAR% node --port 4445
-start java -jar selenium-server-4.21.0.jar node --port 4446
+start java -jar %SELENIUM_JAR% node --port 4446
--- UI-autotests/src/test/java/helpers/scripts/start-grid.sh
+++ UI-autotests/src/test/java/helpers/scripts/start-grid.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-SELENIUM_DIR=Selenium-server-4.21.0
-SELENIUM_JAR=selenium-server-4.21.0.jar
 
+SELENIUM_DIR=selenium-server
+SELENIUM_JAR=selenium-server-4.21.0.jar
 cd $SELENIUM_DIR
 java -jar $SELENIUM_JAR hub
+sleep 5
 java -jar $SELENIUM_JAR node --port 4445
 java -jar $SELENIUM_JAR node --port 4446
