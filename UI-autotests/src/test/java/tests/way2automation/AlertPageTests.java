0bbbb827efda7100bc29423ed7f2a6db0b69401f
Ruslan Bikineev <biken_r@mail.ru>
2024-06-04 09:58:31 +0300

    Ruslan Bikineev: added new alert test

---
+++ UI-autotests/src/test/java/tests/way2automation/AlertPageTests.java
@@ -0,0 +1,30 @@
+package tests.way2automation;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.Story;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.way2automation.AlertPage;
+import tests.BaseTest;
+
+import static data.TestsData.IT_IS_A_CUSTOM_TEXT;
+import static io.qameta.allure.SeverityLevel.NORMAL;
+
+@Epic(value = "Страница с оповещением")
+public class AlertPageTests extends BaseTest {
+    @Test
+    @Feature(value = "Ввод кастомного текста в алерт бокс")
+    @Story(value = "Ввод кастомного текста в алерт бокс и проверка его содержимого")
+    @Owner(value = "Ruslan Bikineev")
+    @Severity(NORMAL)
+    public void testInputAlertBox() {
+        AlertPage alertPage = new AlertPage(getDriver());
+        alertPage.openAlertPage()
+                .submitInputAlert()
+                .sudmitDemonstrateInputBox();
+        Assert.assertTrue(alertPage.getAlertText().contains(IT_IS_A_CUSTOM_TEXT));
+    }
+}
