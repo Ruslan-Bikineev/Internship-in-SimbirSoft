5d1c7bfb84b48e8d304f50b25b38f1dc7c76df5b
Ruslan Bikineev <biken_r@mail.ru>
2024-06-12 21:58:47 +0300

    Ruslan Bikineev: added tests with use cucumber

--- UI-autotests/src/test/java/cucumber/definitions/LoginPageDefinitions.java
+++ UI-autotests/src/test/java/cucumber/definitions/LoginPageDefinitions.java
@@ -1,9 +1,12 @@
 package cucumber.definitions;
 
+import helpers.JavaScriptUtil;
 import io.cucumber.java.After;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java.ru.Дано;
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.То;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -16,7 +19,7 @@ public class LoginPageDefinitions {
     WebDriver driver;
     LoginPage loginPage;
 
-    @Given("Открываем сайт авторизации")
+    @Дано("Открываем сайт авторизации")
     public void openLoginPage() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -27,13 +30,15 @@ public class LoginPageDefinitions {
                 .clickMemberLogin();
     }
 
-    @When("Авторизуемся, логин {string} и пароль {string}")
-    public void loginAs(String login, String password) {
-        loginPage.loginAs(login, password);
+    @Когда("Вводим логин {string} и пароль {string}")
+    public void вводимЛогинЛогинИПарольПароль(String login, String password) {
+        loginPage.typeUsername(login);
+        loginPage.typePassword(password);
     }
 
-    @Then("Проверка, что ничего не происходит и поля остаются пустыми")
-    public void checkLogin() {
+
+    @То("Ничего не происходит и поля ввода остаются пустыми")
+    public void ничегоНеПроисходитИПоляВводаОстаютсяПустыми() {
         Assert.assertEquals("", loginPage.getValueOfEmailField(),
                 "Введенный логин не совпадает с логином в поле ввода");
         Assert.assertEquals("", loginPage.getValuePasswordField(),
@@ -45,4 +50,49 @@ public class LoginPageDefinitions {
         driver.close();
     }
 
+    @И("Убираем фокус с поля ввода пароля")
+    public void убираемФокусСПоляВводаПароля() {
+        JavaScriptUtil.removeFocusFromElement(driver, loginPage.getPasswordField());
+    }
+
+    @То("Под полем ввода логина отображается ошибка {string}")
+    public void подПолемВводаЛогинаОтображаетсяОшибкаОшибкаЛогина(String expectedMessage) {
+        Assert.assertEquals(loginPage.getEmailFieldError(), expectedMessage,
+                "Ошибка под полем ввода логина не соответствует");
+    }
+
+    @То("Под полем ввода пароля отображается ошибка {string}")
+    public void подПолемВводаПароляОтображаетсяОшибкаОшибкаПароля(String expectedMessage) {
+        Assert.assertEquals(loginPage.getPasswordFieldError(), expectedMessage,
+                "Ошибка под полем ввода пароля не соответствует");
+    }
+
+    @То("Нажимаем кнопку авторизации и авторизация не проходит")
+    public void нажимаемКнопкуАвторизацииИАвторизацияНеПроходит() {
+        boolean flag = false;
+        try {
+            loginPage.submitLogin();
+        } catch (TimeoutException e) {
+            flag = true;
+        }
+        Assert.assertTrue(flag);
+    }
+
+    @Когда("Авторизуемся, логин {string} и пароль {string}")
+    public void авторизуемсяЛогинИПароль(String login, String password) {
+        loginPage.loginAs(login, password);
+    }
+
+    @То("Переходим на домашню страницу пользователя {string}")
+    public void переходимНаДомашнюСтраницуПользователя(String urlUserHomePage) {
+        Assert.assertEquals(driver.getCurrentUrl(), urlUserHomePage);
+    }
+
+    @То("Вывод сообщения {string} красного цвета {string}")
+    public void выводСообщенияКрасногоЦвета(String message, String messageColor) {
+        Assert.assertEquals(loginPage.getInvalidLoginMessage(), message,
+                "Вывод сообщения об ошибке авторизации не совпадает");
+        Assert.assertEquals(loginPage.getInvalidLoginMessageColor(), messageColor,
+                "Цвет сообщения об ошибке авторизации не совпадает");
+    }
 }
--- UI-autotests/src/test/java/cucumber/runner/CucumberRunnerTests.java
+++ UI-autotests/src/test/java/cucumber/runner/CucumberRunnerTests.java
@@ -1,10 +1,11 @@
 package cucumber.runner;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(tags = "",
         features = "src/test/resources/features/LoginPage.feature",
         glue = "cucumber.definitions",
-        plugin = {})
+        plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"})
 public class CucumberRunnerTests extends AbstractTestNGCucumberTests {
 }
