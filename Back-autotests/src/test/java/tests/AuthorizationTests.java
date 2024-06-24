7bd705697b2f9946f6b399d1c194e1785ee710b7
Ruslan Bikineev <biken_r@mail.ru>
2024-06-25 00:04:43 +0500

    Ruslan Bikineev: decomposition tests

--- Back-autotests/src/test/java/tests/AuthorizationTests.java
+++ Back-autotests/src/test/java/tests/AuthorizationTests.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static data.TestData.NON_EXISTENT_LOGIN;
@@ -13,8 +14,8 @@ import static data.TestData.VALID_PASSWORD;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 //TODO: added testNg xml file
+//TODO: need decomposition RestAssured
 //TODO: added allure report and annotations
-//TODO: need decomposition tests with DataProvider
 public class AuthorizationTests {
     @Test
     public void testAuthorizationWithValidData() {
@@ -32,77 +33,23 @@ public class AuthorizationTests {
         Assert.assertEquals(response.statusCode(), 200);
     }
 
-    @Test
-    public void testAuthorizationWithValidLoginAndEmptyPassword() {
-        Response response = RestAssured.given()
-                .contentType(ContentType.JSON)
-                .auth()
-                .preemptive()
-                .basic(VALID_LOGIN, "")
-                .when()
-                .get("http://localhost:8000/index.php?rest_route=/wp/v2/users/me")
-                .then()
-                .body(matchesJsonSchemaInClasspath("AuthorizationSchemaStatus500.json"))
-                .extract()
-                .response();
-        Assert.assertEquals(response.statusCode(), 500);
+    @DataProvider(name = "DifferentIncorrectDataProvider")
+    public Object[][] differentIncorrectDataProvider() {
+        return new Object[][]{
+                {"", ""},
+                {"", NON_EXISTENT_PASSWORD},
+                {NON_EXISTENT_LOGIN, ""},
+                {NON_EXISTENT_LOGIN, NON_EXISTENT_PASSWORD}
+        };
     }
 
-    @Test
-    public void testAuthorizationWithEmptyLoginAndValidPassword() {
+    @Test(dataProvider = "DifferentIncorrectDataProvider")
+    public void testAuthorizationWithDifferentIncorrectData(String login, String password) {
         Response response = RestAssured.given()
                 .contentType(ContentType.JSON)
                 .auth()
                 .preemptive()
-                .basic("", VALID_PASSWORD)
-                .when()
-                .get("http://localhost:8000/index.php?rest_route=/wp/v2/users/me")
-                .then()
-                .body(matchesJsonSchemaInClasspath("AuthorizationSchemaStatus500.json"))
-                .extract()
-                .response();
-        Assert.assertEquals(response.statusCode(), 500);
-    }
-
-    @Test
-    public void testAuthorizationWithEmptyData() {
-        Response response = RestAssured.given()
-                .contentType(ContentType.JSON)
-                .auth()
-                .preemptive()
-                .basic("", "")
-                .when()
-                .get("http://localhost:8000/index.php?rest_route=/wp/v2/users/me")
-                .then()
-                .body(matchesJsonSchemaInClasspath("AuthorizationSchemaStatus500.json"))
-                .extract()
-                .response();
-        Assert.assertEquals(response.statusCode(), 500);
-    }
-
-    @Test
-    public void testAuthorizationWithNonExistentData() {
-        Response response = RestAssured.given()
-                .contentType(ContentType.JSON)
-                .auth()
-                .preemptive()
-                .basic(NON_EXISTENT_LOGIN, NON_EXISTENT_PASSWORD)
-                .when()
-                .get("http://localhost:8000/index.php?rest_route=/wp/v2/users/me")
-                .then()
-                .body(matchesJsonSchemaInClasspath("AuthorizationSchemaStatus500.json"))
-                .extract()
-                .response();
-        Assert.assertEquals(response.statusCode(), 500);
-    }
-
-    @Test
-    public void testAuthorizationWithValidLoginAndNonExistentPassword() {
-        Response response = RestAssured.given()
-                .contentType(ContentType.JSON)
-                .auth()
-                .preemptive()
-                .basic(VALID_LOGIN, NON_EXISTENT_PASSWORD)
+                .basic(login, password)
                 .when()
                 .get("http://localhost:8000/index.php?rest_route=/wp/v2/users/me")
                 .then()
