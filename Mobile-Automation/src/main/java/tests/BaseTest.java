7f194d4a3853c63c5732943df19021086572dbac
Ruslan Bikineev <biken_r@mail.ru>
2024-09-13 08:16:11 +0500

    Ruslan Bikineev: added chromedriver Executable path

--- Mobile-Automation/src/main/java/tests/BaseTest.java
+++ Mobile-Automation/src/main/java/tests/BaseTest.java
@@ -25,7 +25,7 @@ public abstract class BaseTest {
         capabilities.setCapability("app", app.getAbsolutePath());
         capabilities.setCapability("automationName", ConfigReader.emulatorConfig.automationName());
         capabilities.setCapability("app-wait-activity", ConfigReader.emulatorConfig.appWaitActivity());
-        capabilities.setCapability("chromedriver_autodownload", true);
+        capabilities.setCapability("chromedriverExecutable", ConfigReader.emulatorConfig.chromedriverExecutable());
         capabilities.setCapability("fullReset", ConfigReader.emulatorConfig.fullReset());
         androidDriver = new AndroidDriver(new URI(ConfigReader.emulatorConfig.remoteURL()).toURL(), capabilities);
         androidDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
